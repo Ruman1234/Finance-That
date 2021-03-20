b19abc3f7a2e6168ada304904e7c509de0a2084a
ammar <>
2021-03-20 18:53:46 +0500

    -- set screens

---
+++ Finance/ViewControllers/Dealer Registration/Home Page/CollectionView/OnBoardingCell/OnBoardingCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  OnBoardingCollectionViewCell.swift
+//  Finance
+//
+//  Created by Ammar Irfan on 19/03/2021.
+//  Copyright © 2021 Ammar Irfan. All rights reserved.
+//
+
+import UIKit
+
+class OnBoardingCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var sellerLbl: UILabel!
+    @IBOutlet weak var financingLbl: UILabel!
+    @IBOutlet weak var img: UIImageView!
+}
--- Finance/ViewControllers/OnBoarding/ViewController.swift
+++ Finance/ViewControllers/OnBoarding/ViewController.swift
@@ -8,67 +8,135 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout{
+    
     @IBOutlet weak var skipBtn: UIButton!
-    @IBOutlet weak var lbl1: UILabel!
-    @IBOutlet weak var lbl2: UILabel!
-    @IBOutlet weak var lbl3: UILabel!
-    @IBOutlet weak var img: UIImageView!
     @IBOutlet weak var view1: UIView!
     @IBOutlet weak var view2: UIView!
     @IBOutlet weak var view3: UIView!
+    @IBOutlet weak var view4: UIView!
+    @IBOutlet weak var blueView: UIView!
     @IBOutlet weak var nextBtn: UIButton!
-        
+    @IBOutlet weak var onBoardCollectionview: UICollectionView!
+    
+    let labels = ["For Seller","For Buyers","For Dealers"]
+    let images = [UIImage(named: "Car"),UIImage(named: "Frame"),UIImage(named: "Group")]
+    let labels2 = ["Sell your vehicle easily with our instant financing!","Finance any vehicle sold by a dealer or private seller!","Take advantage of our marketing to boost your sales!"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-       
-        nextBtn.layer.cornerRadius = 16
+        
+        onBoardCollectionview.delegate = self
+        onBoardCollectionview.dataSource = self
+        
+        nextBtn.layer.cornerRadius = nextBtn.frame.width/2
         view1.layer.backgroundColor = UIColor(red: 0.984, green: 0.318, blue: 0, alpha: 1).cgColor
         view1.layer.cornerRadius = 4
         view2.layer.cornerRadius = 4
         view3.layer.cornerRadius = 4
-
+        blueView.layer.backgroundColor = UIColor(red: 0.967, green: 0.979, blue: 0.988, alpha: 1).cgColor
     }
     
     override func viewWillLayoutSubviews() {
         self.navigationController?.isNavigationBarHidden = true
     }
     
-    @IBAction func skipBtn(_ sender: Any) {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return images.count
+    }
     
-//        let main = self.storyboard?.instantiateViewController(withIdentifier: "UserPage1ViewController") as! UserPage1ViewController
-//        self.navigationController?.pushViewController(main, animated: true)
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "onBoardCell", for: indexPath) as! OnBoardingCollectionViewCell
         
-        let homePageStoryBoard = UIStoryboard(name: "Main", bundle: nil)
+        cell.sellerLbl.text = labels[indexPath.row]
+        cell.financingLbl.text = labels2[indexPath.row]
+        cell.img.image = images[indexPath.row]
+        cell.backgroundColor = UIColor(red: 0.967, green: 0.979, blue: 0.988, alpha: 1)
+        
+        return cell
+    }
+    
+    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+        changeViews()
+    }
+    
+    func changeViews()  {
+        for cell in onBoardCollectionview.visibleCells {
+            let indexPath = onBoardCollectionview.indexPath(for: cell)
+            
+            if indexPath?.row == 0 {
+                view1.layer.backgroundColor = UIColor(red: 0.984, green: 0.318, blue: 0, alpha: 1).cgColor
+                view2.layer.backgroundColor = UIColor(red: 0.82, green: 0.827, blue: 0.839, alpha: 1).cgColor
+                view3.layer.backgroundColor = UIColor(red: 0.82, green: 0.827, blue: 0.839, alpha: 1).cgColor
+            }else if indexPath?.row == 1 {
+                view1.layer.backgroundColor = UIColor(red: 0.984, green: 0.318, blue: 0, alpha: 1).cgColor
+                view2.layer.backgroundColor = UIColor(red: 0.984, green: 0.318, blue: 0, alpha: 1).cgColor
+                view3.layer.backgroundColor = UIColor(red: 0.82, green: 0.827, blue: 0.839, alpha: 1).cgColor
+            }else if indexPath?.row == 2 {
+                view1.layer.backgroundColor = UIColor(red: 0.984, green: 0.318, blue: 0, alpha: 1).cgColor
+                view2.layer.backgroundColor = UIColor(red: 0.984, green: 0.318, blue: 0, alpha: 1).cgColor
+                view3.layer.backgroundColor = UIColor(red: 0.984, green: 0.318, blue: 0, alpha: 1).cgColor
+            }
+        }
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let size = collectionView.frame.size
+        return CGSize(width: size.width, height: 324)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+    }
+    
+    @IBAction func skipBtn(_ sender: Any) {
+//        let homePageStoryBoard = UIStoryboard(name: "HomePage", bundle: nil)
         let main =
-            homePageStoryBoard.instantiateViewController(withIdentifier: "LogInViewController") as! LogInViewController
+            self.storyboard?.instantiateViewController(withIdentifier: "Dealer1ViewController") as! Dealer1ViewController
         self.navigationController?.pushViewController(main, animated: true)
     }
     
     @IBAction func nextBtn(_ sender: Any) {
+        self.onBoardCollectionview.scrollToNextItem {
+            
+        }
         
-        if lbl1.text == "Pictures & Videos" {
-           
-            lbl1.text = "Confused between choices?"
-            lbl2.text = "Compare Cars"
-            lbl3.text = "Compare to choose the right car!"
-            img.image = UIImage(named: "Frame")
+        let visibleRect = CGRect(origin: onBoardCollectionview.contentOffset, size: onBoardCollectionview.bounds.size)
+        let visiblePoint = CGPoint(x: visibleRect.midX, y: visibleRect.midY)
+        let visibleIndexPath = onBoardCollectionview.indexPathForItem(at: visiblePoint)
+        print(visibleIndexPath)
+        let visibleIndexPathRow = visibleIndexPath!.row + 1
+        if visibleIndexPathRow == 0 {
+            view1.layer.backgroundColor = UIColor(red: 0.984, green: 0.318, blue: 0, alpha: 1).cgColor
+            view2.layer.backgroundColor = UIColor(red: 0.82, green: 0.827, blue: 0.839, alpha: 1).cgColor
+            view3.layer.backgroundColor = UIColor(red: 0.82, green: 0.827, blue: 0.839, alpha: 1).cgColor
+        }else if visibleIndexPathRow == 1 {
             view1.layer.backgroundColor = UIColor(red: 0.984, green: 0.318, blue: 0, alpha: 1).cgColor
             view2.layer.backgroundColor = UIColor(red: 0.984, green: 0.318, blue: 0, alpha: 1).cgColor
-
-        }else if lbl1.text == "Confused between choices?"{
-            lbl1.text = "Get best deals"
-            lbl2.text = "Prices and Offers"
-            lbl3.text = "We bring accurate price & best offers to you!"
-            img.image = UIImage(named: "Group")
+            view3.layer.backgroundColor = UIColor(red: 0.82, green: 0.827, blue: 0.839, alpha: 1).cgColor
+        }else if visibleIndexPathRow == 2 {
             view1.layer.backgroundColor = UIColor(red: 0.984, green: 0.318, blue: 0, alpha: 1).cgColor
             view2.layer.backgroundColor = UIColor(red: 0.984, green: 0.318, blue: 0, alpha: 1).cgColor
             view3.layer.backgroundColor = UIColor(red: 0.984, green: 0.318, blue: 0, alpha: 1).cgColor
-
-        }else {
-            let main = self.storyboard?.instantiateViewController(withIdentifier: "UserPage1ViewController") as! UserPage1ViewController
-            self.navigationController?.pushViewController(main, animated: true)
         }
+    }
+    
+}
+extension UICollectionView {
+    func scrollToNextItem(res: ()->()) {
+        let contentOffset = CGFloat(floor(self.contentOffset.x + self.bounds.size.width))
+        if contentOffset > 856{
+            return
+        }
+        self.setContentOffset(CGPoint(x: contentOffset, y: self.contentOffset.y), animated: true)
+        
+        res()
     }
 }
